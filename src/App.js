fed3d6facb82b7b06eb4dde9cda304fefd6d0754
BushraFatimaBF <vglampard@protonmail.com>
2023-02-16 21:20:48 +0000

    set up basic input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [company, setCompany] = useState("");
+  function handleChange(e) {
+    setCompany(e.target.value);
+    console.log("company:", company)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Enter company name
-        </p>
-    
+        <p>Enter company name</p>
+        <input
+          type="text"
+          placeholder="company name here"
+          onChange={handleChange}
+        ></input>
+        <button onClick={getLinkedIn}>LinkedIn</button>
       </header>
     </div>
   );
