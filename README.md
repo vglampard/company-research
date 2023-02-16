796f3c14f16224e98ca8cccf42399258590ae981
BushraFatimaBF <vglampard@protonmail.com>
2023-02-16 22:46:06 +0000

    added more search buttons

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 Features:
 -> open search pages for legal name, glassdoor, linkedin, address, logo, favicon, 
+
+https://www.google.com/search?q=amazon+&biw=2048&bih=994&tbm=nws&sxsrf=AJOqlzXnKhptPFRVhGv0lJK9VYzu3En3cg%3A1676587107327&ei=Y7DuY-7PE_iI9u8PvvudsAE&ved=0ahUKEwiujd7XjZv9AhV4hP0HHb59BxYQ4dUDCA0&uact=5&oq=amazon+&gs_lcp=Cgxnd3Mtd2l6LW5ld3MQAzIECAAQQzIECAAQQzINCAAQgAQQsQMQgwEQCjIECAAQQzIECAAQQzIECAAQQzIKCAAQsQMQgwEQQzIECAAQQzINCAAQgAQQsQMQgwEQCjIECAAQQzoHCAAQsQMQQzoICAAQgAQQsQM6BQgAEIAEUJgCWJgCYL0IaAFwAHgAgAFXiAGsAZIBATKYAQCgAQHAAQE&sclient=gws-wiz-news
+
+https://www.google.com/search?q=amazon+&tbm=isch&ved=2ahUKEwimhrj1jZv9AhXjnCcCHTXZCBIQ2-cCegQIABAA&oq=amazon+&gs_lcp=CgNpbWcQAzIECCMQJzIECCMQJzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDOgUIABCABFC3BVi3BWCYTmgAcAB4AIABW4gBswGSAQEymAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=obDuY-axJeO5nsEPtbKjkAE&bih=981&biw=960
+
+tbm=nws
+
+Can button value be an array?
--- src/App.js
+++ src/App.js
@@ -9,26 +9,44 @@ function App() {
     setCompany(e.target.value);
   }
 
-  const TEXT_SEARCHES = {
-    linkedin: "linkedIn" 
+  //   const TEXT_SEARCHES = {
+  //     linkedin: "linkedIn" ,
+  //     crunchbase: "crunchbase",
+  //     glassdoor: "glassdoor",
+  //   }
+  // const IMAGE_SEARCHES = {
+  //   favicon: "favicon",
+  //   logo: "logo+high+resolution+transparent+background+svg",
+  // }
+
+  function search(company, e) {
+    window.open(
+      `https://www.google.com/search?q=${company}+${e.currentTarget.value}`
+    );
+    console.log("button:", e.currentTarget.value);
   }
-const IMAGE_SEARCHES = {
-favicon: "favicon"
-}
 
-
-  function search(company, e){
-    window.open(`https://www.google.com/search?q=${company}+${e.currentTarget.value}`);
-    console.log("button:", e.currentTarget.value)
+  function imageSearch(company, e) {
+    window.open(
+      `https://www.google.com/search?q=${company}+${e.currentTarget.value}&tbm=isch`
+    );
   }
 
-  function imageSearch(company, e){
-    window.open(`https://www.google.com/search?q=${company}+${e.currentTarget.value}&tbm=isch`)
+  function newsSearch(company, e) {
+    window.open(`https://www.google.com/search?q=${company}&tbm=nws`);
   }
 
-  function searchAll(company, e){
+  //   function searchAll(company){
+  // // take company from input
+  // // call search for every string value in the text search object
+  // // call image search for every string value in image search object
 
-  }
+  // // for (const searchTerm in TEXT_SEARCHES){
+  // //   setTimeout(function(){window.open(`https://www.google.com/search?q=${company}+${searchTerm}&tbm=isch`);
+  // // console.log("ST:", searchTerm)}, 1000)
+  // // }
+
+  //   }
 
   return (
     <div className="App">
@@ -40,17 +58,36 @@ favicon: "favicon"
           placeholder="company name here"
           onChange={handleChange}
         ></input>
-        <button onClick = {()=>searchAll(company)}>Get all! </button>
+        {/* <button onClick = {()=>searchAll(company)}>Get all! </button> */}
         <br></br>
-        <button onClick={(e)=>search(company, e)} value="linkedIn">LinkedIn</button>
-        <button onClick={(e)=>search(company, e)} value="crunchbase">Crunchbase</button>
-        <button onClick={(e)=>search(company, e)} value="glassdoor">Glassdoor</button>
-        <button onClick={(e)=>imageSearch(company, e)} value=
-        "logo+high+resolution+transparent+background+svg">Logo</button>
-             <button onClick={(e)=>imageSearch(company, e)} value=
-        "favicon">Favicon</button>
-
-        </header>
+        <button onClick={(e) => search(company, e)} value="linkedIn">
+          LinkedIn
+        </button>
+        <button onClick={(e) => search(company, e)} value="crunchbase">
+          Crunchbase
+        </button>
+        <button onClick={(e) => search(company, e)} value="glassdoor">
+          Glassdoor
+        </button>
+        <button
+          onClick={(e) => imageSearch(company, e)}
+          value="logo+high+resolution+transparent+background+svg"
+        >
+          Logo
+        </button>
+        <button onClick={(e) => imageSearch(company, e)} value="favicon">
+          Favicon
+        </button>
+        <button onClick={(e) => newsSearch(company, e)} value="news">
+          News
+        </button>
+        <button onClick={(e) => search(company, e)} value="founders">
+          Founders
+        </button>
+        <button onClick={(e) => search(company, e)} value="company+address+hq">
+          Address
+        </button>
+      </header>
     </div>
   );
 }
