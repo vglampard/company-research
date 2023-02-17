f5810c081548c2f4170157f24558ca3658cc348d
BushraFatimaBF <vglampard@protonmail.com>
2023-02-17 14:03:49 +0000

    added more keywords

--- src/ButtonsDisplay/ButtonsDisplay.js
+++ src/ButtonsDisplay/ButtonsDisplay.js
@@ -8,13 +8,13 @@ export default function ButtonsDisplay({ company }) {
     Crunchbase: ["crunchbase", ""],
     Glassdoor: ["glassdoor", ""],
     Logo: ["logo+high+resolution+transparent+background+svg", "&tbm=isch"],
-    Favicon: ["favicon+transparent", "&tbm=isch"],
+    Favicon: ["favicon+transparent-background", "&tbm=isch"],
     News: ["news", "&tbm=nws"],
     Founders: ["all+founders", ""],
     Address: ["company+legal+address+hq", ""],
     Mission: ["company+mission", ""],
     Values: ["company+values", ""],
-    Benefits: ["employee+benefits", ""],
+    Benefits: ["employee+benefits+perks", ""],
     Controversies: ["controversy+scandal+issues+disputes", ""]
   };
 
