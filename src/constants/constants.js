683d5855e89c349433954d6a7c8720b85d6d8cda
BushraFatimaBF <vglampard@protonmail.com>
2023-02-18 14:21:10 +0000

    created searches array of objects in const file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,56 @@
-export const GOOGLE_URL = "https://www.google.com/search?q="
+export const GOOGLE_URL = "https://www.google.com/search?q=";
+
+export const SEARCHES = [
+  {
+    buttonText: "LinkedIn",
+    searchTerms: "linkedin",
+  },
+
+  {
+    buttonText: "Crunchbase",
+    searchTerms: "crunchbase",
+  },
+  {
+    buttonText: "Glassdoor",
+    searchTerms: "glassdoor",
+  },
+  {
+    buttonText: "Logo",
+    searchTerms: "logo+high+resolution+transparent+background+svg",
+    searchType: "&tbm=isch",
+  },
+  {
+    buttonText: "Favicon ",
+    searchTerms: "favicon+transparent+background",
+    searchType: "&tbm=isch",
+  },
+  {
+    buttonText: "News",
+    searchTerms: "company+news",
+    searchType: "&tbm=nws",
+  },
+  {
+    buttonText: "Founders",
+    searchTerms: "all+founders",
+  },
+  {
+    buttonText: "Address",
+    searchTerms: "company+legal+address+hq",
+  },
+  {
+    buttonText: "Mission",
+    searchTerms: "company+mission",
+  },
+  {
+    buttonText: "Values",
+    searchTerms: "company+values",
+  },
+  {
+    buttonText: "Benefits",
+    searchTerms: "employee+benefits+perks",
+  },
+  {
+    buttonText: "Controversies",
+    searchTerms: "controversy+scandal+issues",
+  },
+];
